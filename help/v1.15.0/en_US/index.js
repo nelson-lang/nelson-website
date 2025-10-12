fb7539b1494f5d0e8011bdfb57365212bb48936f
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-10-12 22:03:49 +0200

    doc: french content

--- help/v1.15.0/en_US/index.js
+++ help/v1.15.0/en_US/index.js
@@ -1,5 +1,5 @@
 // index.js - Search index for Nelson help system
-// Auto-generated on 1760281287
+// Auto-generated on 1760298916
 
 // Define the index in a globally accessible variable
 window.searchIndex = [
